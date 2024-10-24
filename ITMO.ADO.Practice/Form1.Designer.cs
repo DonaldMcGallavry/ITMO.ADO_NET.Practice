e204967900717285c6c1287d96d7bdb8a28fc5a9
N.Dubskiy <167004436+DonaldMcGallavry@users.noreply.github.com>
2024-10-24 19:38:09 +0300

    Add files via upload

---
+++ ITMO.ADO.Practice/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace ITMO.ADO.Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.sqlInsertCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlUpdateCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDeleteCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            this.dataSetOrders = new ITMO.ADO.Practice.DataSetOrders();
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.BestEmpBtn = new System.Windows.Forms.Button();
+            this.sqlSelectCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlInsertCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlUpdateCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDeleteCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter2 = new System.Data.SqlClient.SqlDataAdapter();
+            this.dataSetEmp = new ITMO.ADO.Practice.DataSetEmp();
+            this.dataGridViewEmp = new System.Windows.Forms.DataGridView();
+            this.BestEmpText = new System.Windows.Forms.TextBox();
+            this.UpdateButtonOrder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetEmp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "SELECT OrderID, CustomerID, EmployeeID  FROM Orders";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=DESKTOP-D9H2F11;Initial Catalog=Northwind;Integrated Security=True;Po" +
+    "oling=False;Encrypt=True;TrustServerCertificate=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlInsertCommand1
+            // 
+            this.sqlInsertCommand1.CommandText = "INSERT INTO [Orders] ([CustomerID], [EmployeeID]) VALUES (@CustomerID, @EmployeeI" +
+    "D);\r\nSELECT OrderID, CustomerID, EmployeeID FROM Orders WHERE (OrderID = SCOPE_I" +
+    "DENTITY())";
+            this.sqlInsertCommand1.Connection = this.sqlConnection1;
+            this.sqlInsertCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@CustomerID", System.Data.SqlDbType.NChar, 0, "CustomerID"),
+            new System.Data.SqlClient.SqlParameter("@EmployeeID", System.Data.SqlDbType.Int, 0, "EmployeeID")});
+            // 
+            // sqlUpdateCommand1
+            // 
+            this.sqlUpdateCommand1.CommandText = resources.GetString("sqlUpdateCommand1.CommandText");
+            this.sqlUpdateCommand1.Connection = this.sqlConnection1;
+            this.sqlUpdateCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@CustomerID", System.Data.SqlDbType.NChar, 0, "CustomerID"),
+            new System.Data.SqlClient.SqlParameter("@EmployeeID", System.Data.SqlDbType.Int, 0, "EmployeeID"),
+            new System.Data.SqlClient.SqlParameter("@Original_OrderID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "OrderID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_CustomerID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "CustomerID", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_CustomerID", System.Data.SqlDbType.NChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "CustomerID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@OrderID", System.Data.SqlDbType.Int, 4, "OrderID")});
+            // 
+            // sqlDeleteCommand1
+            // 
+            this.sqlDeleteCommand1.CommandText = resources.GetString("sqlDeleteCommand1.CommandText");
+            this.sqlDeleteCommand1.Connection = this.sqlConnection1;
+            this.sqlDeleteCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@Original_OrderID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "OrderID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_CustomerID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "CustomerID", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_CustomerID", System.Data.SqlDbType.NChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "CustomerID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, null)});
+            // 
+            // sqlDataAdapter1
+            // 
+            this.sqlDataAdapter1.DeleteCommand = this.sqlDeleteCommand1;
+            this.sqlDataAdapter1.InsertCommand = this.sqlInsertCommand1;
+            this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
+            this.sqlDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Orders", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("OrderID", "OrderID"),
+                        new System.Data.Common.DataColumnMapping("CustomerID", "CustomerID"),
+                        new System.Data.Common.DataColumnMapping("EmployeeID", "EmployeeID")})});
+            this.sqlDataAdapter1.UpdateCommand = this.sqlUpdateCommand1;
+            // 
+            // dataSetOrders
+            // 
+            this.dataSetOrders.DataSetName = "DataSetOrders";
+            this.dataSetOrders.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(405, 12);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.Size = new System.Drawing.Size(383, 285);
+            this.dataGridViewOrders.TabIndex = 0;
+            // 
+            // BestEmpBtn
+            // 
+            this.BestEmpBtn.Location = new System.Drawing.Point(344, 362);
+            this.BestEmpBtn.Name = "BestEmpBtn";
+            this.BestEmpBtn.Size = new System.Drawing.Size(141, 23);
+            this.BestEmpBtn.TabIndex = 2;
+            this.BestEmpBtn.Text = "CountBestEmployee";
+            this.BestEmpBtn.UseVisualStyleBackColor = true;
+            this.BestEmpBtn.Click += new System.EventHandler(this.BestEmpBtn_Click);
+            // 
+            // sqlSelectCommand2
+            // 
+            this.sqlSelectCommand2.CommandText = "SELECT EmployeeID, LastName, FirstName  FROM Employees";
+            this.sqlSelectCommand2.Connection = this.sqlConnection1;
+            // 
+            // sqlInsertCommand2
+            // 
+            this.sqlInsertCommand2.CommandText = "INSERT INTO [Employees] ([LastName], [FirstName]) VALUES (@LastName, @FirstName);" +
+    "\r\nSELECT EmployeeID, LastName, FirstName FROM Employees WHERE (EmployeeID = SCOP" +
+    "E_IDENTITY())";
+            this.sqlInsertCommand2.Connection = this.sqlConnection1;
+            this.sqlInsertCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 0, "LastName"),
+            new System.Data.SqlClient.SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 0, "FirstName")});
+            // 
+            // sqlUpdateCommand2
+            // 
+            this.sqlUpdateCommand2.CommandText = resources.GetString("sqlUpdateCommand2.CommandText");
+            this.sqlUpdateCommand2.Connection = this.sqlConnection1;
+            this.sqlUpdateCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 0, "LastName"),
+            new System.Data.SqlClient.SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 0, "FirstName"),
+            new System.Data.SqlClient.SqlParameter("@Original_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_LastName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "LastName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_FirstName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "FirstName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@EmployeeID", System.Data.SqlDbType.Int, 4, "EmployeeID")});
+            // 
+            // sqlDeleteCommand2
+            // 
+            this.sqlDeleteCommand2.CommandText = "DELETE FROM [Employees] WHERE (([EmployeeID] = @Original_EmployeeID) AND ([LastNa" +
+    "me] = @Original_LastName) AND ([FirstName] = @Original_FirstName))";
+            this.sqlDeleteCommand2.Connection = this.sqlConnection1;
+            this.sqlDeleteCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@Original_EmployeeID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EmployeeID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_LastName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "LastName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_FirstName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "FirstName", System.Data.DataRowVersion.Original, null)});
+            // 
+            // sqlDataAdapter2
+            // 
+            this.sqlDataAdapter2.DeleteCommand = this.sqlDeleteCommand2;
+            this.sqlDataAdapter2.InsertCommand = this.sqlInsertCommand2;
+            this.sqlDataAdapter2.SelectCommand = this.sqlSelectCommand2;
+            this.sqlDataAdapter2.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Employees", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("EmployeeID", "EmployeeID"),
+                        new System.Data.Common.DataColumnMapping("LastName", "LastName"),
+                        new System.Data.Common.DataColumnMapping("FirstName", "FirstName")})});
+            this.sqlDataAdapter2.UpdateCommand = this.sqlUpdateCommand2;
+            // 
+            // dataSetEmp
+            // 
+            this.dataSetEmp.DataSetName = "DataSetEmp";
+            this.dataSetEmp.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridViewEmp
+            // 
+            this.dataGridViewEmp.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridViewEmp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmp.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewEmp.Name = "dataGridViewEmp";
+            this.dataGridViewEmp.Size = new System.Drawing.Size(387, 285);
+            this.dataGridViewEmp.TabIndex = 3;
+            // 
+            // BestEmpText
+            // 
+            this.BestEmpText.Location = new System.Drawing.Point(314, 323);
+            this.BestEmpText.Name = "BestEmpText";
+            this.BestEmpText.Size = new System.Drawing.Size(202, 20);
+            this.BestEmpText.TabIndex = 4;
+            // 
+            // UpdateButtonOrder
+            // 
+            this.UpdateButtonOrder.Location = new System.Drawing.Point(625, 320);
+            this.UpdateButtonOrder.Name = "UpdateButtonOrder";
+            this.UpdateButtonOrder.Size = new System.Drawing.Size(103, 23);
+            this.UpdateButtonOrder.TabIndex = 5;
+            this.UpdateButtonOrder.Text = "UpdOrder";
+            this.UpdateButtonOrder.UseVisualStyleBackColor = true;
+            this.UpdateButtonOrder.Click += new System.EventHandler(this.UpdateButtonOrder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UpdateButtonOrder);
+            this.Controls.Add(this.BestEmpText);
+            this.Controls.Add(this.dataGridViewEmp);
+            this.Controls.Add(this.BestEmpBtn);
+            this.Controls.Add(this.dataGridViewOrders);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetEmp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlCommand sqlInsertCommand1;
+        private System.Data.SqlClient.SqlCommand sqlUpdateCommand1;
+        private System.Data.SqlClient.SqlCommand sqlDeleteCommand1;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+        private DataSetOrders dataSetOrders;
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.Button BestEmpBtn;
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand2;
+        private System.Data.SqlClient.SqlCommand sqlInsertCommand2;
+        private System.Data.SqlClient.SqlCommand sqlUpdateCommand2;
+        private System.Data.SqlClient.SqlCommand sqlDeleteCommand2;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter2;
+        private DataSetEmp dataSetEmp;
+        private System.Windows.Forms.DataGridView dataGridViewEmp;
+        private System.Windows.Forms.TextBox BestEmpText;
+        private System.Windows.Forms.Button UpdateButtonOrder;
+    }
+}
+
---
+++ ITMO.ADO.Practice/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ITMO.ADO.Practice
+{
+    public partial class Form1 : Form
+    {
+        private static string con = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+        private SqlDataAdapter SqlDataAdapter3;
+        private DataSet NorthwindDataset = new DataSet("Northwind");
+        private DataTable CustomersTable = new DataTable("Employees");
+
+        //private SqlConnection Connection = new SqlConnection(con);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridViewOrders.DataSource = dataSetOrders.Orders;
+            sqlDataAdapter1.Fill(dataSetOrders.Orders);
+
+            dataGridViewEmp.DataSource = dataSetEmp.Employees;
+            sqlDataAdapter2.Fill(dataSetEmp.Employees);
+
+
+        }
+
+        private void BestEmpBtn_Click(object sender, EventArgs e)
+        {
+            CountOrders();
+        }
+
+        private void CountOrders()
+        {
+            //int maxOrderCount = 0;
+            //int employeeIDWithMaxOrders = 0;
+
+            var orderCounts = dataGridViewOrders.Rows.Cast<DataGridViewRow>()
+                .GroupBy(row => row.Cells["EmployeeID"].Value)
+                .Select(g => new { EmployeeID = g.Key, OrderCount = g.Count() })
+                .OrderByDescending(x => x.OrderCount)
+                .FirstOrDefault();
+
+            if (orderCounts != null)
+            {
+                int employeeID = Convert.ToInt32(orderCounts.EmployeeID);
+                string firstName = string.Empty;
+                string lastName = string.Empty;
+
+                foreach (DataGridViewRow row in dataGridViewEmp.Rows)
+                {
+                    if ((int)row.Cells["EmployeeID"].Value == employeeID)
+                    {
+                        firstName = row.Cells["FirstName"].Value.ToString();
+                        lastName = row.Cells["LastName"].Value.ToString();
+                        break;
+                    }
+                }
+
+                string bestEmployeeText = $"{firstName} {lastName} стал сотрудником месяца!";
+                BestEmpText.Text = bestEmployeeText;
+
+                //Добавляем столбец CurrentBestEmp в dataGridViewEmp, если его нет
+                AddColumn();
+
+                // Обновляем столбец CurrentBestEmp
+                foreach (DataGridViewRow row in dataGridViewEmp.Rows)
+                {
+                    if (row.Cells["EmployeeID"]?.Value != null && (int)row.Cells["EmployeeID"].Value == employeeID)
+                    {
+                        row.Cells["CurrentBestEmp"].Value = "*";
+                    }
+                    else
+                    {
+                        row.Cells["CurrentBestEmp"].Value = string.Empty;
+                    }
+                }
+            }
+        }
+
+        private void AddColumn()
+        {
+            if (!dataGridViewEmp.Columns.Contains("CurrentBestEmp"))
+            {
+                dataGridViewEmp.Columns.Add("CurrentBestEmp", "CurrentBestEmp");
+            }
+
+        }
+        private void UpdateButtonOrder_Click(object sender, EventArgs e)
+        {
+            sqlDataAdapter1.Update(dataSetOrders);
+        }
+
+    }
+}
